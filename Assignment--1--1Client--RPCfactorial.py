49e78d6702f8ecb718609aca1219a2147536ed17
vaishnavi5858 <149963594+vaishnavi5858@users.noreply.github.com>
2024-04-28 10:41:55 +0530

    Assignment--1--1Client--RPCfactorial.py

--- Assignment--1--1Client--RPCfactorial.py
+++ Assignment--1--1Client--RPCfactorial.py
@@ -4,3 +4,4 @@ n = int(input("Enter an integer value: "))
 proxy = xmlrpc.client.ServerProxy('http://localhost:8000/')
 result = proxy.factorial(n)
 print(f"The factorial of {n} is {result}")
+#Remote Procedure Call is a software communication protocol that one program can use to request a service from a program located in another computer on a network without having to understand the network's details. RPC is used to call other processes on the remote systems like a local system.
